da6543589cddf5b2f26d18b5a750ac4f80a7daca
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-04 19:56:51 +0200

    Добавьте файлы проекта.

---
+++ Task2.5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть перше число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть третє число: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        double max = FindMax(num1, num2, num3);
+        Console.WriteLine($"Найбільше число: {max}");
+    }
+    static double FindMax(double a, double b, double c)
+    {
+        return Math.Max(Math.Max(a, b), c);
+    }
+}
